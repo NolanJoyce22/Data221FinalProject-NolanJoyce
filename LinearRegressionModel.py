92487584772e22695958a20778f3fbfd211ef13d
NolanJoyce22 <nolan.joyce@ucalgary.ca>
2026-03-18 17:41:50 -0600

    Initialize dataset

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -1,2 +1,9 @@
+import pandas as pd
+from sklearn import train_test_split
+from sklearn.linear_model import LinearRegression
+
+linear_regression_model = LinearRegression(fit_intercept=False)
+
+student_alcohol_data = pd.read_csv("student-mat.csv")
 
 
