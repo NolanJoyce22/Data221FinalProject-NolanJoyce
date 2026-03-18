c6e5bfcd5c5b280a93acaf1b0a7643cf8ea71aa0
NolanJoyce22 <nolan.joyce@ucalgary.ca>
2026-03-18 17:58:35 -0600

    Initialize features and target

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -6,4 +6,16 @@ linear_regression_model = LinearRegression(fit_intercept=False)
 
 student_alcohol_data = pd.read_csv("student-mat.csv")
 
+x = student_alcohol_data[["address", "famsize", "Pstatus", "Medu", "Fedu", "Mjob", "Fjob", "traveltime", "famsup",
+                          "internet", "famrel"]]
+y = student_alcohol_data["G3"]
+
+'''
+One hot code address(U=1, R=0) famsize(LE3 = 1, GT3 = 0), Pstatus(T=1, A=0), Mjob/Fjob(teacher = 1, health = 2, services = 3
+at_home = 4, other = 5), famsup(yes = 1, no = 0), internet(yes=1, no = 0) 
+
+'''
+
+
+
 
