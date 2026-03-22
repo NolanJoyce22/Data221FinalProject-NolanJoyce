90beebaa91c77435d2102f2492a5d619a642014d
NolanJoyce22 <nolan.joyce@ucalgary.ca>
2026-03-22 14:29:35 -0600

    Add comments to previous code, and initialize train-test-split to be used for the model

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -2,27 +2,29 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
-linear_regression_model = LinearRegression(fit_intercept=False)
-
+# Load data set
 student_alcohol_data = pd.read_csv("student-mat.csv")
-
+# Create feature matrix
 x = student_alcohol_data[["address", "famsize", "Pstatus", "Medu", "Fedu", "Mjob", "Fjob", "traveltime", "famsup",
                           "internet", "famrel"]]
+# Create target vector
 y = student_alcohol_data["G3"]
 
-'''
-One hot code address(U=1, R=0) famsize(LE3 = 1, GT3 = 0), Pstatus(T=1, A=0), Mjob/Fjob(teacher = 1, health = 2, services = 3
-at_home = 4, other = 5), famsup(yes = 1, no = 0), internet(yes=1, no = 0) 
-
-'''
-
+# Map features with categorical feature to a numerical variable
 x["address"] = x["address"].map({"U":1, "R":0})
 x["famsize"] = x["famsize"].map({"LE3":1, "GT3":0})
 x["Pstatus"] = x["Pstatus"].map({"T":1, "A":0})
 x["famsup"] = x["famsup"].map({"yes":1, "no":0})
 x["internet"] = x["internet"].map({"yes":1, "no":0})
-
+# One-Hot code features with more than two categorical variables
 x = pd.get_dummies(x, columns=["Mjob", "Fjob"], drop_first=True)
+
+# Create train-test-split for model
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state = 42)
+model = LinearRegression()
+model.fit(x_train, y_train)
+
+
 
 
 
