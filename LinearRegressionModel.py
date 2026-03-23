6d791c2e73dd1f48bc36b33e084e66281541a6fa
NolanJoyce22 <nolan.joyce@ucalgary.ca>
2026-03-23 16:33:50 -0600

    Add scaling for the features

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.preprocessing import StandardScaler
+
 # Load data set
 student_alcohol_data = pd.read_csv("student-mat.csv")
 # Create feature matrix
@@ -21,18 +23,23 @@ x = pd.get_dummies(x, columns=["Mjob", "Fjob"], drop_first=True)
 
 # Create train-test-split for model with 80% training and 20% testing
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state = 42)
-model = LinearRegression()
-model.fit(x_train, y_train)
-# Initialize model and set fit_intercept to......
-linear_regression_model = LinearRegression(fit_intercept= True)
+# Initialize scaler to standardize features
+scaler = StandardScaler()
+# fit the scaler on the training data only to avoid data leakage(mean = 0, std = 1)
+x_train_scaled = scaler.fit_transform(x_train)
+# Use the same scaling parameters to transform the test features
+x_test_scaled = scaler.transform(x_test)
+
+# Initialize LinearRegression model set fit_intercept to True
+model = LinearRegression(fit_intercept=True)
+# Train the model
+model.fit(x_train_scaled, y_train)
 # Predict the target labels
-y_pred = model.predict(x_test)
+y_pred = model.predict(x_test_scaled)
 
 print(y_pred)
 print("R²:", r2_score(y_test, y_pred))
 print("MSE:", mean_squared_error(y_test, y_pred))
-
-print(x.isnull().sum())
 
 
 
